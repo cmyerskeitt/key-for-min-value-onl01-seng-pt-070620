ee64f89f9989d6a403d10cca43d476b9136914fb
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-16 04:59:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,9 @@ def key_for_min_value(name_hash)
   lowest_key = nil 
   lowest_value = nil 
   name_hash.each do |key, value|
-    if lowest_value == nil 
+    if lowest_value == nil || value < lowest value 
+      lowest value == value 
+      lowest key == key 
+    end 
+      lowest key 
 end
